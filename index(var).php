758234cdedb631ca85c6a7400e803424134fc6e1
wildeink <misterjoy@fastmail.com>
2019-12-29 07:04:10 -0500

    first commit of existing site

---
+++ index(var).php
@@ -0,0 +1,3 @@
+<?php
+$OESiteUploadDirectory = "Upload";
+$OEConfWESendMail = '{"WE70ba0e94ab":{"ScriptVersion":210,"ConfSendMail":{"IsUpdated":true,"Type":3,"TypeStr":"SMTP","SpecialType":"","SMTPHost":null,"SMTPPort":25,"SMTPUser":null,"SMTPPassWrd":null,"SMTPSSL":false},"FormLinks":{"ElementsID":{},"ButtonSubmitID":null,"ButtonCancelID":null},"SendMailInfo":{"AdminEmail":{"Items":{"DEFAULT":""}},"CopycacheEmail":{"Items":{"DEFAULT":""}},"CopyEmail":{"Items":{"DEFAULT":""}},"ReturnEmail":null,"ReturnEmailID":null,"Subject":null,"SendReceipt":false,"ReceiptHeader":{"Items":{"DEFAULT":""}},"ReceiptBody":{"Items":{"DEFAULT":""}},"SendReceiptSignature":false,"ReceiptSignature":{"Target":null,"Links":{"Items":{"DEFAULT":""}}},"UserEmailID":null,"ListInputTitle":{}}}}';
